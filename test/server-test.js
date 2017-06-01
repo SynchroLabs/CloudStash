6cf8885517d31b3864cd296f8b0a9aba94057b4b
Robert Dickinson <bob.dickinson@gmail.com>
2017-06-01 12:57:47 -0700

    Add real tests

--- test/server-test.js
+++ test/server-test.js
@@ -4,27 +4,45 @@ var jwt = require('jsonwebtoken');
 
 var mantaBoxServer = require('./../lib/server');
 
+
 var _testSecret = "test";
-var server = mantaBoxServer(_testSecret);
+var _testConfig = require('./../lib/config').getConfig(null, 
+{ 
+    "driver": 
+    { 
+        "provider": "file", 
+        "basePath": "_mantabox_store" 
+    } 
+});
+var server = mantaBoxServer(_testSecret, _testConfig);
 
 var testToken = jwt.sign({ username: "test", userid: 1234 }, _testSecret);
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
-
-describe('GET /user', function() {
-  it('respond with json', function(done) {
+describe('POST /users/get_current_account', function() {
+  it('returns account id', function(done) {
     request(server)
-      .get('/echo/bar')
+      .post('/users/get_current_account')
       .set('Accept', 'application/json')
       .set('Authorization', "Bearer " + testToken)
       .expect('Content-Type', /json/)
-      .expect('"You said: bar"')
+      .expect(function(res){
+           assert.equal(res.body.userid, '1234'); 
+      })
+      .expect(200, done);
+  });
+});
+
+describe('POST /files/download', function() {
+  it('returns file', function(done) {
+    request(server)
+      .post('/files/download')
+      .set('Accept', 'application/octet-stream')
+      .set('Authorization', "Bearer " + testToken)
+      .set('Dropbox-API-Arg', '{ "path": "foo.txt" }')
+      .expect('Content-Type', 'application/octet-stream')
+      .expect(function(res){
+           assert.equal(res.body.toString(), 'Foo is the word'); 
+      })
       .expect(200, done);
   });
 });
